7d4f431318e95487125e869004ceb6957fd184ce
Jessica <duellje@oregonstate.edu>
2020-09-08 11:15:27 -0400

    created add_to_tail method

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -8,3 +8,26 @@ class Linked_List:
     def __init__(self):
         self.head = None  # points to first node in the list
         self.tail = None  # points to last node in the list
+
+    # append
+    def add_to_tail(self, value):
+        # create a new node
+        new_tail = Node(value, None)
+        # check if list is empty
+        if not self.tail:
+            # set self.head and self.tail to new node
+            self.head = new_tail
+            self.tail = new_tail
+        else:
+            # set current tail to new node
+            old_tail = self.tail
+            old_tail.next = new_tail
+            # set self.tail to new node
+            self.tail = new_tail
+
+    # remove
+    def remove_head(self):
+        pass
+
+    def remove_tail(self):
+        pass
